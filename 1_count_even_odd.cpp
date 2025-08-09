6aaba7dec0c044dca709067442648018626eb176
mahmud076-B <smmahmudhasan076@gmail.com>
2025-08-09 23:34:35 +0600

    Update 1_count_even_odd.cpp

--- 1_count_even_odd.cpp
+++ 1_count_even_odd.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
     int n, i, num, even = 0, odd = 0;
+    cout<<"Enter number of elements:";
     cin >> n;
     for (i = 0; i < n; i++) {
+        cout<<"Enter Elements: ";
         cin >> num;
         if (num % 2 == 0) {
             even++;
@@ -13,5 +15,5 @@ int main() {
         }
     }
     cout << "Even: " << even << " Odd: " << odd;
-    return 0;
+    return 0;
 }
