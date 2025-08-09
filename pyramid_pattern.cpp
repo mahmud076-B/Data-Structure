baa804b1d31ca65130246ad6cd6dc40218e564ca
mahmud076-B <smmahmudhasan076@gmail.com>
2025-08-09 23:28:27 +0600

    Update pyramid_pattern.cpp

--- pyramid_pattern.cpp
+++ pyramid_pattern.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main() {
     int rows;
-    cout << "Enter number of rows: ";
+    cout<<"Enter the number of rows:";
     cin >> rows;
-
     for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= (2 * i - 1); j++) {
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
             cout << "*";
         }
         cout << endl;
     }
-    return 0;
+    return 0;
 }
